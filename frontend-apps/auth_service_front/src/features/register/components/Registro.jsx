d17dc9591de913e0262e2de741646b100e65fc74
fgaliano <fgaliano.aguila@gmail.com>
2026-08-07 13:44:06 +0200

    Fix: Se modifica el texto del botón  # 4

--- frontend-apps/auth_service_front/src/features/register/components/Registro.jsx
+++ frontend-apps/auth_service_front/src/features/register/components/Registro.jsx
@@ -291,7 +291,7 @@ const handleSubmit = (e) => {
             GUARDAR USUARIO
           </button>
           <button type="button" className="btn-guardar" onClick={restablecerFormulario}>
-            BORRAR FORMULARIO
+            RESTABLECER FORMULARIO
           </button>          
         </div> 
  
